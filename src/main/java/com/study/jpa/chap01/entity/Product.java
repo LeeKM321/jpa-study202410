9c857096c45c525dcb25c45d724e616b5dd74f15
LeeKM321 <stephen4951@gmail.com>
2024-10-17 11:52:01 +0900

    EntityManager를 이용한 CRUD (영속성 컨텍스트의 특징 확인)

--- src/main/java/com/study/jpa/chap01/entity/Product.java
+++ src/main/java/com/study/jpa/chap01/entity/Product.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
-@Getter @ToString
+@Setter @Getter @ToString
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
