e7e82f7f9f72f319490af91e8f144a3b4eedc816
LeeKM321 <stephen4951@gmail.com>
2024-10-17 14:29:48 +0900

    Spring Data JPA를 이용한 CRUD (PK로 조건절 사용)

--- src/main/java/com/study/jpa/chap01/repository/ProductRepository.java
+++ src/main/java/com/study/jpa/chap01/repository/ProductRepository.java
@@ -4,9 +4,14 @@ import com.study.jpa.chap01.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // JpaRepository를 상속한 후 첫번째 제네릭에 엔터티클래스 타입.
 // 두번째에 PK의 타입을 작성합니다.
 public interface ProductRepository
         extends JpaRepository<Product, Long> {
+
+    void deleteByName(String name);
+
 
 }
