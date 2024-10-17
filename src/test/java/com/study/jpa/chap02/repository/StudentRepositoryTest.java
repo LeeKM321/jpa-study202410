720829a1768846f8e1e75d36aefb425edc2a8948
LeeKM321 <stephen4951@gmail.com>
2024-10-17 14:51:14 +0900

    chap02 Entity, Repository, Test 클래스 생성

---
+++ src/test/java/com/study/jpa/chap02/repository/StudentRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.study.jpa.chap02.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
