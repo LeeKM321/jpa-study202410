9e5095f03072218d02bb3581747ec557bce88a79
LeeKM321 <stephen4951@gmail.com>
2024-10-17 15:29:39 +0900

    메서드 이름으로 쿼리를 조합하는 쿼리메서드 방식 사용

--- src/main/java/com/study/jpa/chap02/repository/StudentRepository.java
+++ src/main/java/com/study/jpa/chap02/repository/StudentRepository.java
@@ -3,6 +3,39 @@ package com.study.jpa.chap02.repository;
 import com.study.jpa.chap02.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, String> {
 
+    List<Student> findByName(String name);
+
+    List<Student> findByCityAndMajor(String city, String major);
+
+    // WHERE major LIKE '%major%'
+    List<Student> findByMajorContaining(String major);
+
+    // WHERE major LIKE 'major%'
+    List<Student> findByMajorStartingWith(String major);
+
+    // WHERE major LIKE '%major'
+    List<Student> findByMajorEndingWith(String major);
+
+    // WHERE age >= ?
+//    List<Student> findNameAndMajorByAgeGreaterThanEqual(int age);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
